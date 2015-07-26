2d4d04f97bd83147a53b6d052e96f4ac28c1ae94
pointofpresence <git@pointofpresence.ru>
2015-07-26 18:00:16 +0600

    Reversed

---
+++ tracker/js/common.js
@@ -0,0 +1,30 @@
+/* -*- mode: javascript; tab-width: 2; indent-tabs-mode: nil; -*-
+*
+* Copyright (c) 2011-2013 Marcus Geelnard
+*
+* This file is part of SoundBox.
+*
+* SoundBox is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* SoundBox is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with SoundBox.  If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+
+function include(filename)
+{
+	var head = document.getElementsByTagName('head')[0];
+	var s = document.createElement('script');
+	s.type = 'text/javascript';
+	s.src = filename;
+	head.appendChild(s);
+}
+
